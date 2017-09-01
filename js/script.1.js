e7cb12be14fe73310dc5767b28ebaf84f21fe003
vitalik.antosyuk <vitalikantosyuk@gmail.com>
2017-09-01 17:03:27 +0300

    remove document.write, add console.log

--- js/script.1.js
+++ js/script.1.js
@@ -2,4 +2,4 @@ var name = prompt ('Enter name','Mr.Somebody');
     event = prompt('Enter event','anywhere');
     time = prompt('Enter time','be when');
     place = prompt('Enter place','the universe');
-document.write(name+' has a '+event+' today at '+time+' somewhere in '+place+".");
+console.log(name+' has a '+event+' today at '+time+' somewhere in '+place+".");
